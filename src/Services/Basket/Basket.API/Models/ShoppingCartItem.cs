94630ac7b94d1f0270f6fe47fde795a3a25dc885
Yahya Eraltay <eraltay35@gmail.com>
2026-02-21 14:05:05 +0300

    Basket.API: Added ShoppingCartItem model

---
+++ src/Services/Basket/Basket.API/Models/ShoppingCartItem.cs
@@ -0,0 +1,10 @@
+namespace Basket.API.Models;
+
+public class ShoppingCartItem
+{
+    public Guid ProductId { get; set; }
+    public int Quantity { get; set; }
+    public string Color { get; set; }
+    public decimal Price { get; set; }
+    public string ProductName { get; set; }
+}
