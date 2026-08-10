2cdee5d1a3d8330f3af1e694a989862d0809e117
Yahya Eraltay <eraltay35@gmail.com>
2026-08-10 19:09:11 +0300

    Ordering: Created dependency injection files

--- src/Services/Ordering/Ordering.API/Program.cs
+++ src/Services/Ordering/Ordering.API/Program.cs
@@ -1,4 +1,14 @@
+using Ordering.API;
+using Ordering.Application;
+using Ordering.Infrastructure;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services
+    .AddApplicationServices(builder.Configuration)
+    .AddInfrastructureServices(builder.Configuration)
+    .AddApiServices(builder.Configuration);
+
 var app = builder.Build();
 
 app.Run();
