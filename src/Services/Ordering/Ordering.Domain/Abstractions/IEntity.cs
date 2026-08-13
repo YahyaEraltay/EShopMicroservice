4b430a428f9c3b33a4f9dc5c6ee6931080ca87ad
Yahya Eraltay <eraltay35@gmail.com>
2026-08-13 18:52:34 +0300

    Ordering-Domain: Created all models

--- src/Services/Ordering/Ordering.Domain/Abstractions/IEntity.cs
+++ src/Services/Ordering/Ordering.Domain/Abstractions/IEntity.cs
@@ -7,8 +7,8 @@ public interface IEntity<T> : IEntity
 
 public interface IEntity
 {
-    public DateTime CreatedAt { get; set; }
+    public DateTime? CreatedAt { get; set; }
     public string CreatedBy { get; set; }
-    public DateTime UpdatedAt { get; set; }
+    public DateTime? UpdatedAt { get; set; }
     public string UpdatedBy { get; set; }
 }
