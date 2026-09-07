0bcad228e3ae729fd7ca75bd33702327d855fed7
Yahya Eraltay <eraltay35@gmail.com>
2026-09-07 23:24:57 +0300

    Ordering-Infrastructure: Created auditable entity interceptors

--- src/Services/Ordering/Ordering.Infrastructure/DependencyInjection.cs
+++ src/Services/Ordering/Ordering.Infrastructure/DependencyInjection.cs
@@ -1,17 +1,16 @@
-﻿using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.DependencyInjection;
-using Ordering.Infrastructure.Data;
+﻿namespace Ordering.Infrastructure;
 
-namespace Ordering.Infrastructure;
 public static class DependencyInjection
 {
-    public static IServiceCollection AddInfrastructureServices
-        (this IServiceCollection services, IConfiguration configuration)
+    public static IServiceCollection AddInfrastructureServices(this IServiceCollection services, IConfiguration configuration)
     {
         var connectionString = configuration.GetConnectionString("Database");
 
-        services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(connectionString));
+        services.AddDbContext<ApplicationDbContext>((options) =>
+       {
+           options.AddInterceptors(new AuditableEntityInterceptor());
+           options.UseSqlServer(connectionString);
+       });
 
         return services;
     }
---
+++ src/Services/Ordering/Ordering.Infrastructure/GlobalUsing.cs
@@ -0,0 +1,8 @@
+global using Microsoft.EntityFrameworkCore;
+global using Microsoft.Extensions.Configuration;
+global using Microsoft.Extensions.DependencyInjection;
+global using Ordering.Infrastructure.Data;
+global using Ordering.Infrastructure.Data.Interceptors;
+global using Microsoft.EntityFrameworkCore.ChangeTracking;
+global using Microsoft.EntityFrameworkCore.Diagnostics;
+global using Ordering.Domain.Abstractions;
